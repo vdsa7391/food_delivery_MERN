1d252e1dd4c57a9eb21992565f7d0b2f04181034
vdsa7391 <khushboorathore621@gmail.com>
2024-10-26 04:23:24 +0200

    cahnges for render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),commonjs(), nodeResolve()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+  },
+  build: {
+    minify: 'esbuild', // alternatively, try 'terser' if Rollup parsing fails
+    sourcemap: false,
+  }
+
 });
