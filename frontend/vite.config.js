54181a7dc8e7ffce237158829ca4aa426f649f0e
vdsa7391 <khushboorathore621@gmail.com>
2024-10-26 12:25:38 +0200

    vite common js removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,20 +3,23 @@ import react from '@vitejs/plugin-react'
 
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from 'vite-plugin-commonjs';
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),commonjs({
-    include: [/node_modules/],
-  }), nodeResolve()],
+  plugins: [react(),commonjs(), nodeResolve()],
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
   },
   build: {
     minify: 'esbuild', // alternatively, try 'terser' if Rollup parsing fails
     sourcemap: false,
-  }
+  },
+  resolve: {
+    alias: {
+      react: require.resolve('react'),
+    },
+  },
 
 });
